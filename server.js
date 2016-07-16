e23a69a19c9cf77eaad3994d6a5642ecf3878d57
racifuina <racifuina@me.com>
2016-07-16 10:12:26 -0600

    First Commit, some front end and some backend.

---
+++ server.js
@@ -0,0 +1,42 @@
+//Amazon Web Services
+var AWS = require("aws-sdk");
+
+//Express server
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+app.use(express.static('public'));
+
+//AWS & DynamoDB
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: "https://dynamodb.us-east-1.amazonaws.com",
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/public/dashboard.html');
+});
+
+//Socket.io Service
+io.on("connection", function (socket) {
+    console.log("New socket.io client");
+
+
+
+
+});
+
+
+
+
+
+http.listen(8080, "192.168.0.14", function () {
+    console.log("Web Server Started.");
+});
+
+process.on("uncaughtException", function (err) {
+    console.log("uncaughtException", err);
+});
