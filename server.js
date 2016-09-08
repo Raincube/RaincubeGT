a68132cefd9f74938b51706654469c5104941420
Rigoberto Acifuina <developer@Developers-MacBook-Air.local>
2016-09-08 14:04:30 -0600

    connection

--- server.js
+++ server.js
@@ -154,11 +154,11 @@ net.createServer(function (connection) {
         } else {
             console.log("GOOD DATA");
             console.log(1, "send");
-            setTimeout(function(){deviceConnected.write("O");}, 2);
+            setTimeout(function(){connection.write("O");}, 2);
             console.log(2, "send");
-            setTimeout(function(){deviceConnected.write("O");}, 2);
+            setTimeout(function(){connection.write("O");}, 2);
             console.log(3, "send");
-            setTimeout(function(){deviceConnected.write("O");}, 2);
+            setTimeout(function(){connection.write("O");}, 2);
 
             newMonitorInfo(data_str);
 
